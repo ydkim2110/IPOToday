4e1e6f50800e1f04b0210a2d6f340a69011e6dd2
LeeChungWoo <chung92@rencar.co.kr>
2021-06-22 23:32:02 +0900

    add ui alarm fragment

--- app/src/main/java/com/ipotoday/ipotoday/ui/alarm/AlarmFragment.kt
+++ app/src/main/java/com/ipotoday/ipotoday/ui/alarm/AlarmFragment.kt
@@ -4,21 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.ipotoday.ipotoday.R
 import com.ipotoday.ipotoday.databinding.FragmentAlarmBinding
 import com.ipotoday.ipotoday.ui.base.BaseFragment
 
 class AlarmFragment : BaseFragment() {
+    private lateinit var  binding:FragmentAlarmBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return FragmentAlarmBinding.inflate(inflater, container, false).root
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_alarm, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.title = "알림목록"
     }
 
     companion object {
